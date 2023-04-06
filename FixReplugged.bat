741c15f9cf8ccd4226ac8f4750d589ceea1d2a65
Motz <motzumoto@lunardev.group>
2023-04-06 17:25:23 -0400

    upload

---
+++ FixReplugged.bat
@@ -0,0 +1,174 @@
+@echo off
+setlocal
+
+:check_connection
+cls
+echo Checking for internet connection...
+ping -n 1 google.com >nul 2>&1
+if not %errorlevel% == 0 (
+  echo No internet connection detected. Waiting 15 seconds before trying again.
+  timeout /t 15 >nul 2>&1
+  goto check_connection
+) else (
+  echo Internet connection detected. Proceeding with script.
+)
+
+:check_nodejs
+echo Checking if Node.js is installed...
+where npm >nul 2>&1
+if %errorlevel% == 0 (
+  echo Node.js is already installed.
+  goto check_replugged
+)
+echo Node.js is not installed on this system.
+choice /c yn /m "Do you want to install Node.js now?"
+if errorlevel 2 (
+  echo Exiting script.
+  goto :eof
+)
+goto install_nodejs
+
+:install_nodejs
+echo Downloading Node.js installer...
+bitsadmin /transfer "nodejs_installer" /download /priority high "https://nodejs.org/dist/v18.13.0/node-v18.13.0-x64.msi" "%temp%\node-v18.13.0-x64.msi"
+echo Installing Node.js...
+start "" /wait msiexec /i "%temp%\node-v18.13.0-x64.msi" /qn
+if %ERRORLEVEL% == 0 (
+  echo Node.js installation successful.
+  goto check_nodejs
+)
+echo Node.js installation failed. Exiting script.
+start "" "https://nodejs.org/en/download/"
+goto :eof
+
+
+:check_replugged
+echo Checking if replugged is installed...
+cd %userprofile%\replugged
+if not exist "%userprofile%\replugged" (
+  choice /c yn /m "replugged is not installed on this system. Do you want to install it now?"
+  if errorlevel 2 (
+    echo Exiting script.
+    goto :eof
+  ) else (
+    goto install_replugged
+  )
+) else (
+  echo replugged is already installed.
+  goto check_discord
+)
+
+:install_replugged
+echo Installing replugged...
+cd %userprofile%
+if not exist "%userprofile%\replugged" (
+  git clone https://github.com/replugged-org/replugged
+)
+cd "%userprofile%/replugged" 
+call pnpm i 
+call pnpm build
+if not %errorlevel% == 0 (
+  echo Failed to install replugged. Exiting script.
+  goto :eof
+) else (
+  echo replugged installation successful.
+  goto check_discord
+)
+
+
+:check_discord
+set /p discordversion="Which discord version you want to install replugged on? (stable,ptb,canary,development) [stable]: " /t 20
+if "%discordversion%" == "" (set discordversion=stable)
+
+
+if /i "%discordversion%" == "stable" (
+  echo Stopping Discord process...
+  taskkill /f /im discord.exe >nul 2>&1
+  if not %errorlevel% == 0 (
+    echo Failed to stop Discord process.
+  )
+) else if /i "%discordversion%" == "ptb" (
+  echo Stopping DiscordPTB process...
+  taskkill /f /im discordptb.exe >nul 2>&1
+  if not %errorlevel% == 0 (
+    echo Failed to stop DiscordPTB process.
+  )
+) else if /i "%discordversion%" == "canary" (
+  echo Stopping DiscordCanary process...
+  taskkill /f /im discordcanary.exe >nul 2>&1
+  if not %errorlevel% == 0 (
+    echo Failed to stop DiscordCanary process.
+  )
+) else if /i "%discordversion%" == "development" (
+  echo Stopping DiscordDevelopment process...
+  taskkill /f /im discorddevelopment.exe >nul 2>&1
+  if not %errorlevel% == 0 (
+    echo Failed to stop DiscordDevelopment process.
+  )
+) else (
+  echo Invalid input. Exiting script.
+  goto :eof
+)
+
+echo Installing pnpm...
+call npm i -g pnpm >nul 2>&1
+if not %errorlevel% == 0 (
+  echo Failed to install pnpm.
+)
+
+echo Changing to %userprofile%\replugged directory...
+PUSHD %userprofile%\replugged >nul 2>&1
+if not %errorlevel% == 0 (
+  echo Failed to change to %userprofile%\replugged directory.
+)
+
+echo Updating global git configuration...
+call git config --global --add safe.directory %userprofile%\replugged >nul 2>&1
+if not %errorlevel% == 0 (
+  echo Failed to update global git configuration.
+)
+
+echo Pulling latest changes from Git repository...
+call git pull >nul 2>&1
+if not %errorlevel% == 0 (
+  echo Failed to pull latest changes from Git repository.
+)
+
+echo Unplugging %discordversion%...
+call pnpm run unplug %discordversion% >nul 2>&1
+if not %errorlevel% == 0 (
+  echo Failed to unplug %discordversion%.
+)
+
+echo Installing dependencies...
+call pnpm i >nul 2>&1
+if not %errorlevel% == 0 (
+  echo Failed to install dependencies.
+)
+echo Building project...
+call pnpm run build >nul 2>&1
+if not %errorlevel% == 0 (
+  echo Failed to build project.
+)
+
+echo Plugging %discordversion%...
+call pnpm run plug %discordversion% >nul 2>&1
+if not %errorlevel% == 0 (
+  echo Failed to plug %discordversion%.
+)
+
+echo Launching %discordversion% update process...
+if /i "%discordversion%" == "stable" (
+  START "" "%localappdata%\Discord\Update.exe" --processStart discord.exe
+) else if /i "%discordversion%" == "ptb" (
+  START "" "%localappdata%\DiscordPTB\Update.exe" --processStart discordptb.exe
+) else if /i "%discordversion%" == "canary" (
+  START "" "%localappdata%\DiscordCanary\Update.exe" --processStart discordcanary.exe
+) else if /i "%discordversion%" == "development" (
+  START "" "%localappdata%\DiscordDevelopment\Update.exe" --processStart discorddevelopment.exe
+)
+
+echo Restoring original current directory...
+POPD
+
+echo Done.
---
+++ README.md
@@ -0,0 +1,25 @@
+# Do-Replugged
+A batch script designed to automate the process of installing and building replugged for your discord platforms
+
+
+# What it does
+Checks if you have an internet connection and will wait until you have one if you don't have said internet connection. 
+Checks if you have node.js installed. 
+  If you dont have node installed it will ask you if you want to install it. 
+    if you select yes the script will then get the node webpage and get the installer and install the application for you. 
+  if you select no, the script will exit. 
+the script will ask you which discord platform you're on. 
+it will then try to kill your discord client. 
+installs pnpm. 
+updates git config to make the directory safe (probably not needed). 
+pulls updates from the replugged repo. 
+unplugs from your client and installs dependencies. 
+builds replugged.  
+plugs back into your client. 
+launches your client for you.  
+
+# How to contribute
+Thats easy, you can star the repo or add your own lines of code if you have suggestions.
+
+# Join my Discord server
+Join [here](https://discord.gg/cNRNeaX)
